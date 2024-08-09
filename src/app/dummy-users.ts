90f10f197873b1b59b05eeb2c54635549d49af84
pawel-pawel <pawelitwojcik@gmail.com>
2024-08-09 22:16:18 +0200

    header and users

---
+++ src/app/dummy-users.ts
@@ -0,0 +1,32 @@
+export const DUMMY_USERS = [
+  {
+    id: 'u1',
+    name: 'Adam Nowak',
+    avatar: 'user-1.jpg',
+  },
+  {
+    id: 'u2',
+    name: 'Andrzej Duda',
+    avatar: 'user-2.jpg',
+  },
+  {
+    id: 'u3',
+    name: 'Zbigniew Boniek',
+    avatar: 'user-3.jpg',
+  },
+  {
+    id: 'u4',
+    name: 'Adam Małysz',
+    avatar: 'user-4.jpg',
+  },
+  {
+    id: 'u5',
+    name: 'Iga Świątek',
+    avatar: 'user-5.jpg',
+  },
+  {
+    id: 'u6',
+    name: 'Mariusz Pudzianowski',
+    avatar: 'user-6.jpg',
+  },
+];
